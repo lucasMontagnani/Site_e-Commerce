8c3d943e4ff94e26c58ee1a1f24e0d54eb4e2d1d
Lucas Montagnani Calil Elias <lucascalilelias@yahoo.com.br>
2021-09-19 17:47:54 -0300

    Update links.php

--- php/Page Parts/links.php
+++ php/Page Parts/links.php
@@ -1,9 +1,9 @@
 	<!-- Link Bootstrap -->
 	<link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
 	<!-- CSS link do Menu Header-->
-	<link rel="stylesheet" type="text/css" href="../css/hedasrsursrtcaralhosk.css">
+	<link rel="stylesheet" type="text/css" href="../css/menu_header.css">
     <!-- CSS link do Footer-->
-    <link rel="stylesheet" type="text/css" href="../css/fotosinporrakct.css">
+    <link rel="stylesheet" type="text/css" href="../css/fotos.css">
      <!-- CSS link do To The Top Button-->
     <link rel="stylesheet" type="text/css" href="../css/to_the_top.css">
 
